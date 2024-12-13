544ea4a88ef51fb29d53df416f3ce820bc184dcc
YukiNasumi <ireneenriquez657@gmail.com>
2024-12-13 22:32:32 +0800

    修改处理数据文件的bug，在生成dataset时候不直接生成词表而是允许使用一个公共词表。 增加了用全连接层训练的例子

--- data_process.py
+++ data_process.py
@@ -18,7 +18,7 @@ class emotionDataset(Dataset):
         self.review = [ s.split()[:max_len] for s in data['review']]#truncnate
         #print(torch.tensor(vocab[self.review])[0])
         self.tokens =  pad_sequence([torch.tensor(token) for token in vocab[self.review]],batch_first=True,padding_value=0)# 将每个句子编码后填充
-        self.label = data['label']
+        self.label = [label for label in data['label']]
         self.max_len = max_len
     
     def __len__(self):
@@ -27,7 +27,9 @@ class emotionDataset(Dataset):
     def __getitem__(self,index):
         return self.tokens[index],torch.tensor(self.label[index])
 
-def gen_dataset(data,max_len=600):
-    return emotionDataset(gen_vocab(data),data,max_len)
+def gen_dataset(data,vocab = None,max_len=600):
+    if not vocab:
+        vocab = gen_vocab(data)
+    return emotionDataset(vocab,data,max_len)
     
     
