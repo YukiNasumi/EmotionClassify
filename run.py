c84e25f1c7297575427412631abc63371525bd30
YukiNasumi <ireneenriquez657@gmail.com>
2025-01-14 21:41:05 +0800

    organize code files

--- run.py
+++
@@ -1,6 +0,0 @@
-import os
-zipName = '~/LastExp1.zip'
-os.system(f'python GPT2.py --name model20')
-os.system(f'zip -r {zipName} /root/EmotionClassify')
-os.system(f'oss cp {zipName} oss://')
-os.system('shutdown')
